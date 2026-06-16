45f6e7f9024379a3583fa01eb7a223868d67b7e5
ztytotoro <ztytotoro@outlook.com>
2026-06-16 18:14:25 +0800

    chore: bump beta installer to v0.2.0-beta.7

--- deploy/site/public/install.sh
+++ deploy/site/public/install.sh
@@ -2,7 +2,7 @@
 set -eu
 
 REPO="${SIKONG_RELEASE_REPO:-lidessen/sikong}"
-VERSION="${SIKONG_VERSION:-v0.2.0-beta.6}"
+VERSION="${SIKONG_VERSION:-v0.2.0-beta.7}"
 ASSET="sikong-darwin-arm64.tar.gz"
 
 os="$(uname -s)"
